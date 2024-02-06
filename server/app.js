0235914f0c7c9cbbc62df0decaa2e36cc7230740
errijahi sumeja <errijahi.sumeja.21@size.ba>
2024-02-06 10:51:42 +0100

    added register-user

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/userRoutes');
+var usersRouter = require('./routes/users');
 
 var doetenv = require('dotenv');
 doetenv.config();
@@ -43,10 +43,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-
-
-
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
